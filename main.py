267afeaf17407ffce53eb3cfad3bc67816fdd856
RidiculouslyPro <bransonmurphey@gmail.com>
2025-04-12 23:53:54 -0700

    Update main.py
    Fix render port environment

--- main.py
+++ main.py
@@ -26,5 +26,5 @@ def get_trending():
     return jsonify([x['topic'] for x in sorted_topics[:5]])
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get('PORT', 10000))
     app.run(host='0.0.0.0', port=port)
